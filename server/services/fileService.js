7623a2c121c9529c6073bbc0c1b3d68e7ed83936
Judy Tat <tatjudy@live.com>
2021-02-26 20:16:13 -0700

    update

--- server/services/fileService.js
+++ server/services/fileService.js
@@ -1,49 +1,27 @@
+
+  
 /* 
        Read and Write File Utility
        Commonjs Modules
        exports.funcName = ()=>{}
-
        import
        require('module name)
     
 */
 
-const fs = require('fs');
-const path = require('path');
-const { v4: uuidv4 } = require('uuid');
-
-
-// exports.getFileContents = (filePath) => {
-//     const contentFilePath = fs.readFileSync(path.join(__dirname, filePath));
-//     return fileContents;
-// }
+const fs = require('fs')
+const path = require('path')
 
 
 exports.getFileContents = (filePath)=>{
-   let fileContents = JSON.parse(fs.readFileSync(path.join(__dirname, filePath)));
-   //console.log(fileContents.username);
-   return fileContents;
+   let fileContents = JSON.parse(fs.readFileSync(path.join(__dirname, filePath)))
+   return fileContents
      
 }
-                   
-    // {"id": "1aae33ef-a55b-4874-9e06-398d7b881ec2", "username": "jane", "email": "jane@work.com", "password": "123"}
-           
-
-getFileContents('../data/users.json');
-let userID = uuidv4();
 
 exports.writeFileContents = (filePath, data) =>{
-
-    let fileContents = fs.readFileSync(path.join(__dirname, filePath));
-    fileContents = JSON.parse(fileContents);
-    //fileContents = JSON.stringify(fileContents);
-    fileContents.push(data);
-    fileContents = JSON.stringify(fileContents);
-    fs.writeFileSync(path.join(__dirname, filePath), fileContents);
+    let fileContents = getFileContents(filePath)
+    fileContents.push(data)
+    fileContents = JSON.stringify(fileContents)
+    fs.writeFileSync(path.join(__dirname, filePath), fileContents)
 }
-//writeFileContents('../data/users.json', {id: userID, username:"john", email:"john@work.com", password: "321" });
- 
-
-// [
-//     {"id": "1aae33ef-a55b-4874-9e06-398d7b881ec2", "username": "jane", "email": "jane@work.com", "password": "123"}
-// ]
