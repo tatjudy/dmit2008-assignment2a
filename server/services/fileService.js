d219510de447f46f9e2a8d806dc90a9a1ffac3f3
Judy Tat <tatjudy@live.com>
2021-02-26 20:22:25 -0700

    update

--- server/services/fileService.js
+++ server/services/fileService.js
@@ -9,19 +9,22 @@
     
 */
 
-const fs = require('fs')
-const path = require('path')
-
+const fs = require('fs');
+const path = require('path');
+const { v4: uuidv4 } = require('uuid');
+let userID = uuidv4();
 
 exports.getFileContents = (filePath)=>{
-   let fileContents = JSON.parse(fs.readFileSync(path.join(__dirname, filePath)))
-   return fileContents
+   let fileContents = JSON.parse(fs.readFileSync(path.join(__dirname, filePath)));
+   return fileContents;
      
 }
 
 exports.writeFileContents = (filePath, data) =>{
-    let fileContents = getFileContents(filePath)
-    fileContents.push(data)
-    fileContents = JSON.stringify(fileContents)
-    fs.writeFileSync(path.join(__dirname, filePath), fileContents)
+    let fileContents = getFileContents(filePath);
+    fileContents.push(data);
+    fileContents = JSON.stringify(fileContents);
+    fs.writeFileSync(path.join(__dirname, filePath), fileContents);
 }
+
+// writeFileContents('../data/users.json', {id: userID, username:"jane", email:"jane@work.com", password: "4321" });
