329b85077265cf20ba015261f4d18cef14f72386
Judy Tat <tatjudy@live.com>
2021-02-26 19:39:35 -0700

    update

--- server/index.js
+++ server/index.js
@@ -1,49 +1,46 @@
 // require dotenv package to read the properties in the .env file.
-// never upload .env file to git.
-require('dotenv').config()
-//import the express module
+require('dotenv').config();
+//import express module
 const express = require('express');
 // import the path utils from Node.
-const path = require('path')
-const cors = require('cors')
-const cookSession = require('cookie-session')
+const path = require('path');
+const cors = require('cors');
+const cookSession = require('cookie-session');
 
 // Importing our Login Service Used With the POST Login Route
-const loginService = require('./services/loginService')
+const loginService = require('./services/loginService');
 
 
 
 // create an instance of express
-const app = express()
+const app = express();
  
 // read the value of PORT NODE_EVN variable in the .env file
 // when the index.js file starts up this file is read in and
 // we can set configuration variables for the application.
 // never upload to git...
-const PORT =  process.env.PORT || 5000 
+const PORT =  process.env.PORT || 5000 ;
 
  
 // Middleware For Cross Origin Resource SHaring
-app.use(cors())
+app.use(cors());
 
 //To get access to the name value pairs send in the message Body of POST Request.
- app.use(express.urlencoded({extended:true}))
- app.use(express.json())
+ app.use(express.urlencoded({extended:true}));
+ app.use(express.json());
 
  // Session Middleware
  app.use(cookSession({
    name:"session",
    keys:['SDFLU9iw2308dlsfuwe2adfl', 'LDFA34gsdfgFOPW2323DA7FS2']
- }))
+ }));
 
  // Setup Template Engine
- app.set('view engine', 'ejs')
- app.set('views', path.join(__dirname, './views'))
+ app.set('view engine', 'ejs');
+ app.set('views', path.join(__dirname, './views'));
  
 
 //Middleware Serving Static Pages from client directory
-// second parameter is an configuration object of how we want
-// the static file server to run.
  
 app.use(express.static(path.join(__dirname, "../client"), {extensions: ["html", 'htm']})
 );
@@ -55,30 +52,30 @@ app.use(express.static(path.join(__dirname, "../client"), {extensions: ["html", 
  // Tell Express that you want to access POST Request body
  // Setup   app.use(express.urlencoded({extended:true}))
 
- // Basic Example of a Protected Route
+ // Protected Route
  app.get('/dashboard', (req, res)=>{
           if(req.session.isValid){
-            res.render('dashboard')
+            res.render('dashboard');
           }else{
-           res.redirect('/login')
+           res.redirect('/login');
           }
- })
+ });
 
  app.get('/login', (req, res)=>{
    // user template placed inside the views directory
    // res.render(view, data)   ejs.render(template, {data})
    res.render('login', {passwordWarning:"", emailWarning:"", email:"", password:""})
 
- })
+ });
  app.post('/login', (req, res)=>{
    // if your incomming name value pairs are alot then create an object
     const credentials = {
       email:req.body.email,
       password:req.body.password
-    }
+    };
     // isValidUser returns {user:null, emailWarning, passwordWarning}
     // isValudUser.user !=null...
-    const isValidUser =  loginService.authenticate(credentials)
+    const isValidUser =  loginService.authenticate(credentials);
    
        //if the isValidUser has a user returned
        if( isValidUser.user !== null){
@@ -96,9 +93,9 @@ app.use(express.static(path.join(__dirname, "../client"), {extensions: ["html", 
              passwordWarning:isValidUser.passwordWarning,
              email:req.body.email,
              password:req.body.password
-            })
+            });
        }
-  })
+  });
     
  
  app.post('/login', (req, res)=>{
@@ -106,12 +103,12 @@ app.use(express.static(path.join(__dirname, "../client"), {extensions: ["html", 
    const credentials = {
      email:req.body.email,
      password:req.body.password
-    }
+    };
     
     
-    const isValidUser = loginService.authenticate(credentials)
+    const isValidUser = loginService.authenticate(credentials);
    
-    res.end()
+    res.end();
  
  })
 
@@ -129,5 +126,5 @@ app.use((req, res) => {
 
 // Tell express app to listen for incomming request on a specific PORT
 app.listen(PORT, () => {
-  console.log(`server started on http://localhost:5000`);
+  console.log(`The server has started on http://localhost:5000`);
 });
--- server/services/fileService.js
+++ server/services/fileService.js
@@ -8,24 +8,42 @@
     
 */
 
-const fs = require('fs')
-const path = require('path')
+const fs = require('fs');
+const path = require('path');
+const { v4: uuidv4 } = require('uuid');
 
 
-exports.getFileContents = (filePath)=>{
-   let fileContents = JSON.parse(fs.readFileSync(path.join(__dirname, filePath)))
-   return fileContents
+// exports.getFileContents = (filePath) => {
+//     const contentFilePath = fs.readFileSync(path.join(__dirname, filePath));
+//     return fileContents;
+// }
+
+
+const getFileContents = (filePath)=>{
+   let fileContents = JSON.parse(fs.readFileSync(path.join(__dirname, filePath)));
+   //console.log(fileContents.username);
      
 }
+                   
+    // {"id": "1aae33ef-a55b-4874-9e06-398d7b881ec2", "username": "jane", "email": "jane@work.com", "password": "123"}
+           
 
-exports.writeFileContents = (filePath, data) =>{
-    let fileContents = getFileContents(filePath)
-    fileContents.push(data)
-    fileContents = JSON.stringify(fileContents)
-    fs.writeFileSync(path.join(__dirname, filePath), fileContents)
+getFileContents('../data/users.json');
+let userID = uuidv4();
+
+const writeFileContents = (filePath, data) =>{
+
+    let fileContents = fs.readFileSync(path.join(__dirname, filePath));
+    fileContents = JSON.parse(fileContents);
+    //fileContents = JSON.stringify(fileContents);
+    fileContents.push(data);
+    fileContents = JSON.stringify(fileContents);
+    fs.writeFileSync(path.join(__dirname, filePath), fileContents);
+
 }
-
-
+writeFileContents('../data/users.json', {id: userID, username:"john", email:"john@work.com", password: "321" });
  
 
- 
+// [
+//     {"id": "1aae33ef-a55b-4874-9e06-398d7b881ec2", "username": "jane", "email": "jane@work.com", "password": "123"}
+// ]
